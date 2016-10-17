262a26ca5e1f773b7ba0bf4136528258ad7246f1
DannyLenoir <dannylenoir@gmail.com>
2016-10-17 21:06:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
 (function(){
- var volkswagenApp = angular.module('volkswagenapp',[]);
             
-            
-            volkswagenApp.controller('VolkswagenCtrl', [ '$http' , function($http){
-             var vw =  this;
-             vw.gegevens = [];
-             $http.get('autos.json').success(function(data){
-             vw.gegevens = data;
-             });
+var volkswagenApp = angular.module('volkswagenapp', []);
 
-            
 
-            }]);
-            
-            volkswagenApp.controller('FormController',function(){
-               this.gegevens={};
-               var vw = this;
-               vw.gegevens = [];
-              /* this.addGegevens = function(gegeven) {
-                gegeven.gegevens.push(this.gegeven);
-                this.gegevens={};
-               }*/
-               this.addGegevens = function(gegeven){
-                this.gegevens.datum = Date.now();
-                vw.gegevens.push(this.gegeven);
-                this.gegevens = {};
-               }
-               
-                        
-                    });
-          
-               
+volkswagenApp.controller('VolkswagenCtrl', ['$http', '$rootScope', function ($http, $rootScope) {
+        var vw = this;
+        vw.gegevens = [];
+        $http.get('autos.json').success(function (data) {
+            vw.gegevens = data;
+        });
+        $rootScope.VolksWagenAppScope = vw;
+       
+
+
+    }]);
+
+volkswagenApp.controller('FormController', ['$rootScope', function ($rootScope, $http) {
+        this.gegevens = {};
+   
+        this.addGegevens = function (gegeven) {
+            debugger;
+            this.gegevens.datum = Date.now();
+            $rootScope.VolksWagenAppScope.gegevens.push(this.gegevens);
+            this.gegevens = {};
+        }
+       
+       
+
+    }]);
                 
           
 
